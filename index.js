a6d0aae44b21549ea90211399cc643d6933284ba
Ratha <ratharaksmey2@gmail.com>
2024-08-21 22:31:47 +0700

    ready

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const addUser = (id, first_name, last_name, email) => {
 
 }
 addUser(1, 'Un', 'Ratha', 'rathaun123@gmail.com');
-
-console.log(users); // [{ id: 1, name: 'Un' }, 
+addUser(2, 'Rosamond', 'Twelvetree', 'rtwelvetree0@prnewswire.com');
+addUser(3, 'North', 'Keble', 'nkeble1@marriott.com');
+addUser(4, 'Astra', 'Toderbrugge', 'atoderbrugge2@plala.or.jp');
+addUser(5, 'Wilie', 'Daltry', 'wdaltry3@hexun.com');
+addUser(6, 'Libby', 'McGillivrie', 'lmcgillivrie4@gnu.org ');
+addUser(7, 'Bertina', 'Perulli', 'bperulli5@hugedomains.com');
+addUser(8, 'Yorker', 'Itzakson', 'yitzakson6@webnode.com');
+addUser(9, 'Ralf', 'Bruna', 'rbruna7@bandcamp.com');
+addUser(10, 'Shell', 'Matiewe', 'smatiewe8@zimbio.com');
+console.log(users); // [{ id: 1, first_name: 'Un',last_name'Ratha',email'Ratha123@gmail.com' }, 
 
 const find_user = id => {
     // finish this function here
@@ -18,7 +26,6 @@ const find_user = id => {
  // Example usage
  const foundUser = find_user(0);
  console.log(foundUser); // { id: 1, name: 'Un' }
-
 
 // DO NOT REMOVE THIS
 export {users, find_user}
